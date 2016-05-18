2c022ba5131d29978eeec13acdac9e1eb27b82f8
ybarshchevskyi <ybarshchevskyi@ndi.org>
2016-05-18 20:15:22 +0300

    add_concat_css_plugin

--- gulpfile.js
+++ gulpfile.js
@@ -2,3 +2,10 @@ var gulp = require('gulp');
 var concatCSS = require('gulp-concat-css');
 var cleanCSS = require('gulp-clean-css');
 var htmlIncluder = require('gulp-htmlincluder');
+
+//Merge css
+gulp.task('merge', function(){
+    gulp.src('dev/css/*.css')
+          .pipe(concatCSS('style.css'))
+          .pipe(gulp.dest('build/'))
+          })
