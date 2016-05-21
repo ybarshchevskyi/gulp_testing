2b1eb394622ceefd45ae71f60fa4162fcbe1927d
ybarshchevskyi <ybarshchevskyi@ndi.org>
2016-05-21 12:20:49 +0300

    Add gulp plugins

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,29 @@
 var gulp = require('gulp');
 var concatCSS = require('gulp-concat-css');
 var cleanCSS = require('gulp-clean-css');
-var htmlIncluder = require('gulp-htmlincluder');
+var includer = require('gulp-htmlincluder');
+var connect = require('gulp-connect');
+var livereload = require('livereload');
 
-//Merge css
-gulp.task('merge', function(){
+//Merge and clean css
+gulp.task('mergeFile', function(){
     gulp.src('dev/css/*.css')
           .pipe(concatCSS('style.css'))
+        .pipe(cleanCSS({compatibility: 'ie8'}))
           .pipe(gulp.dest('build/'))
           })
+
+//Include HTML files into one file
+gulp.task('html', function(){
+      gulp.src('dev/**/*.html')
+    	.pipe(includer())
+        .pipe(gulp.dest('build/'))
+})
+
+//Gulp connect
+gulp.task('connect', function() {
+  connect.server({
+    root: 'build',
+    livereload: true
+  });
+});
