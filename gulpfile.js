378eddbcd4bb585811128c06e04b4541da55b50d
ybarshchevskyi <ybarshchevskyi@ndi.org>
2016-05-23 15:35:37 +0300

    Dead branch

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,21 @@
 var gulp = require('gulp');
 var concatCSS = require('gulp-concat-css');
 var cleanCSS = require('gulp-clean-css');
-var htmlIncluder = require('gulp-htmlincluder');
+var includer = require('gulp-htmlincluder');
 
 //Merge css
 gulp.task('merge', function(){
     gulp.src('dev/css/*.css')
           .pipe(concatCSS('style.css'))
+ .pipe(cleanCSS({compatibility:'ie8'}))
           .pipe(gulp.dest('build/'))
           })
+
+// Include HTML
+ 
+gulp.task('html', function() {
+    gulp.src('dev/**/*.html')
+    	.pipe(includer())
+        .pipe(gulp.dest('build/'));
+});
+
